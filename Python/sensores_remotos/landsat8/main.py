ae193de177fa92c511986c714d63edd5ff199f19
salviega <salviega6@gmail.com>
2022-01-07 08:24:51 -0500

    update

--- Python/sensores_remotos/landsat8/main.py
+++ Python/sensores_remotos/landsat8/main.py
@@ -4,16 +4,16 @@ from landsat import landsatAPI
 # Site's coord (EPSG:4326)
 latitude = 5.48333
 longitude = -75.0667
-geojson = "/Users/santiagoviana/Desktop/SIG/PNNSFL/Imagenes satelitales/map.geojson" # https://geojson.io/
+geojson = "xxx" # https://geojson.io/
 footprint = landsat.Footprint(latitude, longitude, geojson)
 
 # USGS website
-username = 'salviega6'
-password = '6y2w4r3e3e1q9o'
+username = 'xxx'
+password = 'xxx'
 
 # Download folder
-donwload_folder = "/Users/santiagoviana/Downloads"
+donwload_folder = "xxx"
 
 # Download imagen Landsat 8 
 api = landsatAPI(username, password)
-api.query(footprint, ("01/01/2017", "07/31/2017"), donwload_folder) # date start & date end
+api.query(footprint, ("xxx", "xxx"), donwload_folder) # date start & date end
